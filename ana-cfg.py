669911b6567ff8e3b8df2ec533cbcecd035dabdf
tomeichlersmith <eichl008@umn.edu>
2025-10-02 11:09:06 -0500

    allow limiting number of events for testing

--- ana-cfg.py
+++ ana-cfg.py
@@ -1,6 +1,13 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('input_file', nargs='+',help='input ROOT files to study')
+parser.add_argument('-n','--n-events',type=int,default=-1,help='maximum number of events to study')
+parser.add_argument('-o','--output',default='hist.root',help='output file to write histograms into')
+args = parser.parse_args()
+
 from LDMX.Framework import ldmxcfg
 p = ldmxcfg.Process('ana')
 p.sequence = [ ldmxcfg.Analyzer.from_file('Tracking4EaTStudy.cxx') ]
-import sys
-p.inputFiles = sys.argv[1:]
-p.histogramFile = 'hist.root'
+p.inputFiles = args.input_file
+p.histogramFile = args.output
+p.maxEvents = args.n_events
